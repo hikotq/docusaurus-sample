66030f4741192733d1c1d660cfb52e7a73b68c56
hikotq <25645304+hikotq@users.noreply.github.com>
2025-05-21 16:24:31 +0900

    swagger-uiのページを追加

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -115,6 +115,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {to: '/openapi', label: 'redoc', position: 'left'},
+	{to: '/swagger', label: 'Swagger', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
